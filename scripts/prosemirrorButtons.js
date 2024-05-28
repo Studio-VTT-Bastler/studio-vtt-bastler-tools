cb1126703ad13e08081f2249ba174ab59e56569c
Lyynix <luxfabianl@gmail.com>
2024-05-28 09:40:36 +0200

    added new classes to buttons

--- scripts/prosemirrorButtons.js
+++ scripts/prosemirrorButtons.js
@@ -1,6 +1,6 @@
 export function initProsemirrorDSA5() {
   Hooks.on("getProseMirrorMenuDropDowns", (menu, items) => {
-    const wrapIn = foundry.prosemirror.commands.wrapIn;
+    const wrapIn = ProseMirror.commands.wrapIn;
     if ("format" in items) {
       items.format.entries.push({
         action: "svttb-styles",
@@ -19,13 +19,61 @@ export function initProsemirrorDSA5() {
             },
           },
           {
-            action: "readaloud",
-            title: "Read aloud",
+            action: "scenetitle",
+            title: "Scene title",
             node: menu.schema.nodes.div,
-            attrs: { class: "readaloud" },
+            attrs: { class: "scenetitle" },
             cmd: () => {
               menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "readaloud" } },
+                attrs: { _preserve: { class: "scenetitle" } },
+              });
+              return true;
+            },
+          },
+          {
+            action: "dsalist",
+            title: "DSA list",
+            node: menu.schema.nodes.div,
+            attrs: { class: "dsalist" },
+            cmd: () => {
+              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+                attrs: { _preserve: { class: "dsalist" } },
+              });
+              return true;
+            },
+          },
+          {
+            action: "dsalist2",
+            title: "Alt. DSA list",
+            node: menu.schema.nodes.div,
+            attrs: { class: "dsalist2" },
+            cmd: () => {
+              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+                attrs: { _preserve: { class: "dsalist2" } },
+              });
+              return true;
+            },
+          },
+          {
+            action: "maskfield",
+            title: "Mask Field",
+            node: menu.schema.nodes.div,
+            attrs: { class: "maskfield" },
+            cmd: () => {
+              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+                attrs: { _preserve: { class: "maskfield" } },
+              });
+              return true;
+            },
+          },
+          {
+            action: "maskfield2",
+            title: "Alt. Mask Field",
+            node: menu.schema.nodes.div,
+            attrs: { class: "maskfield2" },
+            cmd: () => {
+              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+                attrs: { _preserve: { class: "maskfield2" } },
               });
               return true;
             },
