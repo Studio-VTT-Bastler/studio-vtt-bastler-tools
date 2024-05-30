428fff5aa0c916af3fab7053096eb6d72d69db9d
Lyynix <luxfabianl@gmail.com>
2024-05-30 14:37:20 +0200

    added comments to script

--- scripts/prosemirrorButtons.js
+++ scripts/prosemirrorButtons.js
@@ -1,90 +1,109 @@
 import { log } from "./script.js";
 
+/**
+ * This Function initializes ProseMirror Buttons for the dsa5 system
+ */
 export function initProsemirrorDSA5() {
+  // "getProseMirrorMenuDropDowns" Hook gets called, when ProseMirror Editor
+  // is rendered, to give access to dropdown items
   Hooks.on("getProseMirrorMenuDropDowns", (menu, items) => {
+    // retrieve foundry-function for wrapping prosemirror text in html nodes
     const wrapIn = ProseMirror.commands.wrapIn;
-    log(menu)
-    if ("format" in items) {
-      items.format.entries.push({
-        action: "svttb-styles",
-        title: "SVTTB Styles",
-        children: [
-          {
-            action: "readaloud",
-            title: "Read aloud",
-            node: menu.schema.nodes.div,
-            attrs: { class: "readaloud" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "readaloud" } },
-              });
-              return true;
-            },
+
+    // return if no format is in dropdown
+    if (!("format" in items)) return;
+
+    // add item to format dropdown
+    items.format.entries.push({
+      action: "svttb-styles",
+      title: "SVTTB Styles",
+      // define child items of "SVTTB Styles"
+      children: [
+        {
+          action: "readaloud",
+          title: "Read aloud",
+          node: menu.schema.nodes.div,
+          attrs: { class: "readaloud" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "readaloud" } },
+            });
+            return true;
           },
-          {
-            action: "scenetitle",
-            title: "Scene title",
-            node: menu.schema.nodes.div,
-            attrs: { class: "scenetitle" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "scenetitle" } },
-              });
-              return true;
-            },
+        },
+        {
+          action: "scenetitle",
+          title: "Scene title",
+          node: menu.schema.nodes.div,
+          attrs: { class: "scenetitle" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "scenetitle" } },
+            });
+            return true;
           },
-          {
-            action: "dsalist",
-            title: "DSA list",
-            node: menu.schema.nodes.div,
-            attrs: { class: "dsalist" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "dsalist" } },
-              });
-              menu._toggleBlock(menu.schema.nodes.bullet_list, wrapIn)
-              return true;
-            },
+        },
+        {
+          action: "dsalist",
+          title: "DSA list",
+          node: menu.schema.nodes.div,
+          attrs: { class: "dsalist" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "dsalist" } },
+            });
+            // use function ProseMirrorMenu._toggleBlock() to additionally wrap whole Block in Bullet List
+            // actually would be better to use function wrapInList(), but there is no access in scope
+            menu._toggleBlock(menu.schema.nodes.bullet_list, wrapIn);
+            return true;
           },
-          {
-            action: "vttblist",
-            title: "VTTB List",
-            node: menu.schema.nodes.div,
-            attrs: { class: "vttblist" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "vttblist" } },
-              });
-              menu._toggleBlock(menu.schema.nodes.bullet_list, wrapIn)
-              return true;
-            },
+        },
+        {
+          action: "vttblist",
+          title: "VTTB List",
+          node: menu.schema.nodes.div,
+          attrs: { class: "vttblist" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "vttblist" } },
+            });
+            // use function ProseMirrorMenu._toggleBlock() to additionally wrap whole Block in Bullet List
+            // actually would be better to use function wrapInList(), but there is no access in scope
+            menu._toggleBlock(menu.schema.nodes.bullet_list, wrapIn);
+            return true;
           },
-          {
-            action: "maskfield",
-            title: "Mask Field",
-            node: menu.schema.nodes.div,
-            attrs: { class: "maskfield" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "maskfield" } },
-              });
-              return true;
-            },
+        },
+        {
+          action: "maskfield",
+          title: "Mask Field",
+          node: menu.schema.nodes.div,
+          attrs: { class: "maskfield" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "maskfield" } },
+            });
+            return true;
           },
-          {
-            action: "gmnotes",
-            title: "GM Notes",
-            node: menu.schema.nodes.div,
-            attrs: { class: "gmnotes" },
-            cmd: () => {
-              menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
-                attrs: { _preserve: { class: "gmnotes" } },
-              });
-              return true;
-            },
+        },
+        {
+          action: "gmnotes",
+          title: "GM Notes",
+          node: menu.schema.nodes.div,
+          attrs: { class: "gmnotes" },
+          cmd: () => {
+            // use function ProseMirrorMenu._toggleBlock() to wrap whole Block in Node
+            menu._toggleBlock(menu.schema.nodes.div, wrapIn, {
+              attrs: { _preserve: { class: "gmnotes" } },
+            });
+            return true;
           },
-        ],
-      });
-    }
+        },
+      ],
+    });
   });
 }
