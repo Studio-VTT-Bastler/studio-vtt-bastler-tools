5d431a6d4fb8c9a13e284d21128eddcdf64aadbe
Eric J. Francois <ejfr76@gmail.com>
2024-06-16 20:58:04 +0200

    Added resizing version of readaloud in DSA5 styles. Replaced media query for gmnotes with a resize observer in the script to properly determine the available width within a window.

--- scripts/script.js
+++ scripts/script.js
@@ -52,6 +52,57 @@ Hooks.on("ready", () => {
       );
       log("inserted custom dsa5 styles");
 
+      /*  The following code serves to observe when gmnotes elements are resized and add the 'narrow' class if the width
+          is below a certain threshold, so that the mask can be hidden.
+          This is necessary since CSS media queries only observe the viewport width and not the element width.
+          Since the gmnotes are created dynamically, we also need to observe the document for changes and add the observer
+          to any new elements (e.g. when a journal window is opened).
+          NOTE: I noticed that the mutation observer breaks Prosemirror functionality to enter into source code mode. Hence
+          I've limited it to normal journal display, while in the editor always the verion without the mask is shown. */
+
+      let handleResize = function(entries) {
+        entries.forEach(entry => {
+          if (entry.contentRect.width <= 350) {
+            entry.target.classList.add('narrow');
+          } else {
+            entry.target.classList.remove('narrow');
+          }
+        });
+      }
+
+      // Create a ResizeObserver instance with the callback function
+      const resizeObserver = new ResizeObserver(handleResize);
+
+      // Function to observe a single element
+      let observeElement = function(element) {
+        resizeObserver.observe(element);
+      }
+
+      // Get all current gmnotes elements and observe them
+      document.querySelectorAll('.gmnotes').forEach(observeElement);
+
+
+      // Create a MutationObserver to observe changes in the DOM
+      const mutationObserver = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          mutation.addedNodes.forEach(node => {
+            if (node.nodeType === 1 && node.matches('.journal-sheet-container .gmnotes')) {
+              observeElement(node);
+            }
+            // Also check for any descendants with the .gmnotes class
+            node.querySelectorAll?.('.gmnotes').forEach(observeElement);
+          });
+        });
+      });
+
+      // Observe the document body for added nodes
+      mutationObserver.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+
+      /* --- end of gmnotes resize observer --- */
+
       // init ProseMirror Buttons if enabled
       if (
         game.settings.get("studio-vtt-bastler-tools", "use-prosemirror-buttons")
