5c2f0f5f61316104ec48b5c000f3206763b72ef0
Shadows_eye <71041904+shadows-eye@users.noreply.github.com>
2025-05-23 22:31:50 +0200

    v13_city_menu
    a start_not on v13 yet.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,3 +5,18 @@
 export function log(...o) {
   console.log("SVTTB |", ...o);
 }
+export const registerBasicHelpers = () => {
+    Handlebars.registerHelper('for', function (from, to, options) {
+            let accum = '';
+            for (let i = from; i < to; i += 1) {
+                accum += options.fn(i);
+            }
+    
+            return accum;
+    });
+    // Register a Handlebars helper called 'capitalizeFirst'
+    Handlebars.registerHelper('capitalizeFirst', function (string) {
+        if (typeof string !== 'string' || !string) return '';
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    });
+};
